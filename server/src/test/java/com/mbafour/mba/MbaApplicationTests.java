b97bcd4de1880a8a0a3475fa66cd7e02c9838c0e
genius-jo <joks7285@daum.net>
2022-01-26 23:49:49 +0900

    add : React, Spring Boot, MySQL Docker development environment

---
+++ server/src/test/java/com/mbafour/mba/MbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbafour.mba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
