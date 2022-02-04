85ecb7ad550f6bf601c6136a90b26a0c90e87d4d
genius-jo <joks7285@daum.net>
2022-02-05 01:50:42 +0900

    feature : Get current user information

---
+++ server/src/main/java/com/mbafour/mba/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.mbafour.mba.controller;
+
+import com.mbafour.mba.dto.ApiResult;
+import com.mbafour.mba.service.MemberStatusService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@ResponseBody
+@RequestMapping("/member")
+@AllArgsConstructor
+public class MemberController {
+
+    private final MemberStatusService memberStatusService;
+
+    @GetMapping(value= "/status")
+    public ApiResult<?> memberStatus(HttpServletRequest request){
+        return ApiResult.OK(memberStatusService.getStatus(request));
+    }
+}
