843ae54efa207a2226d02ebd2873d3fde6c592dc
yxxung <https://github.com/yxxung>
2022-02-14 13:47:26 +0900

    fix: react-router-version

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 //BrowserRouter, HashRouter: 페이지를 나눠준다.
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
       <App />  
-    </HashRouter>,
+    </BrowserRouter>,
   document.getElementById("root")
 );
